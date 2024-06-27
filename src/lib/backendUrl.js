d6925981d4dd2a3898052b2cd0d298de8ff2b222
highscoretech <highscoretechng@gmail.com>
2024-06-27 09:15:30 +0400

    making

--- src/lib/backendUrl.js
+++ src/lib/backendUrl.js
@@ -4,7 +4,7 @@ export const serverUrl = () => {
     if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
       return "http://localhost:8000";
     }else{
-      return "https://backend-1-mbvq.onrender.com";
+      return "https://cyclix-game.onrender.com";
     }
   }
 };
